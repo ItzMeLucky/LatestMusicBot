702cd70ee3719fd1a868e958a10417f04d3e5ea0
ItzMeLucky <97837254+ItzMeLucky@users.noreply.github.com>
2022-01-16 21:49:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/LUCKYRAJPUTOP/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/ItzMeLucky/NewMusicBot /NewMusicBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/ItzMeLucky/NewMusicBot -b $BRANCH /NewMusicBot
 fi
-cd /VCPlayerBot
+cd /NewMusicBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
