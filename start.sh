4a5b0178872ba44535edf8fbb2328e240ed6a479
ItzMeLucky <97837254+ItzMeLucky@users.noreply.github.com>
2022-02-01 16:21:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/ItzMeLucky/OldMusicBot /OldMusicBot
+  git clone https://github.com/ItzMeLucky/LatestMusicBot /LatestMusicBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/ItzMeLucky/OldMusicBot -b $BRANCH /OldMusicBot
+  git clone https://github.com/ItzMeLucky/LatestMusicBot -b $BRANCH /LatestMusicBot
 fi
-cd /OldMusicBot
+cd /LatestMusicBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
